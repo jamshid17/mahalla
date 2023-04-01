14856b6ebcd506bfe7d258a6bdf0eb264b3155bf
jamshid17 <jamshidjabbarov17@gmail.com>
2023-04-01 13:38:58 +0500

    asd

--- core/settings.py
+++ core/settings.py
@@ -163,6 +163,9 @@ AUTH_USER_MODEL = 'users.User'
 
 STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_FILES_DIR = [
+    '/root/projects/mahalla/static'
+]
 
 MEDIA_URL = "media/"
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
--- core/urls.py
+++ core/urls.py
@@ -23,4 +23,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('main.urls')),
     path('users/', include('users.urls')),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) 
+# + static(settings.STATUS_URL, document_root=settings.STATUS_ROOT)
