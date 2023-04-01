758155f8ec1b17b54527ca719f4bf8e57cf63b04
jamshid17 <jamshidjabbarov17@gmail.com>
2023-04-01 14:44:44 +0500

    g

--- core/settings.py
+++ core/settings.py
@@ -163,6 +163,7 @@ AUTH_USER_MODEL = 'users.User'
 
 STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS = [BASE_DIR / "static"]
 
 MEDIA_URL = "media/"
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
