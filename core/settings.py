5cee3a79119446644a238c77d57321d6e2961a74
jamshid17 <jamshidjabbarov17@gmail.com>
2023-04-01 13:02:26 +0500

    root

--- core/settings.py
+++ core/settings.py
@@ -161,6 +161,8 @@ AUTH_USER_MODEL = 'users.User'
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
 MEDIA_URL = "media/"
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
